77a5b01c14c2e9ce58c58f7d7586da12b6991608
Adrian Flejszer <adrianflejszer@gmail.com>
2017-05-30 10:55:15 +0200

    Initial Commit

---
+++ Hello world/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+        char[] digits = {1,2};
+        /*if (digits.length != 10)
+        {
+            System.out.println("zla liczba cyfr!");
+            return;
+        }*/
+        int sum = 0;
+        for (int i = 1; i <= digits.length; i++)
+        {
+            sum += i * digits[i-1];
+        }
+
+        if (sum % 11 == 0)
+        {
+            System.out.println(sum);
+            System.out.println("prawdilowo!");
+            return;
+        }
+        else
+        {
+            System.out.println(sum);
+            System.out.println("nieprawidlowo");
+            return;
+        }
+        
+    }
+}
