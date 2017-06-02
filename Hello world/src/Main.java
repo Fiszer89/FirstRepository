2aa3ff440990e3288e68c29dd1c4cf87eb15ba23
Adrian Flejszer <adfl@netcompany.com>
2017-06-02 14:57:09 +0200

    Commit second

--- Hello world/src/Main.java
+++ Hello world/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        int j = 1;
         char[] digits = {1,2,3,4,5,6,7,8,9,1,2};
         if (digits.length != 10)
         {
