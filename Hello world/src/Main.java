9e8f5dcae36e969ec2d56d5f867ccb195f572ed6
Adrian Flejszer <adrianflejszer@gmail.com>
2017-06-02 13:41:25 +0200

    First commit

--- Hello world/src/Main.java
+++ Hello world/src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        char[] digits = {1,2};
-        /*if (digits.length != 10)
+        char[] digits = {1,2,3,4,5,6,7,8,9,1,2};
+        if (digits.length != 10)
         {
             System.out.println("zla liczba cyfr!");
             return;
-        }*/
+        }
         int sum = 0;
         for (int i = 1; i <= digits.length; i++)
         {
