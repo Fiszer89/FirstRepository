a5faf5565b6738d60a4a491d804ded2cdb8a61b5
Adrian Flejszer <adfl@netcompany.com>
2017-06-05 12:51:17 +0200

    branch commit

--- Hello world/src/Main.java
+++ Hello world/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         int j = 1;
+        int k = 0;
         char[] digits = {1,2,3,4,5,6,7,8,9,1,2};
         if (digits.length != 10)
         {
